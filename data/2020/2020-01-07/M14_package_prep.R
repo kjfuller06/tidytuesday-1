f01051121aa14d8363c0a1f2d31cec8d9893ef35
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-01-10 12:17:53 +1100

    working on cleaning

---
+++ data/2020/2020-01-07/M14_package_prep.R
@@ -0,0 +1,7 @@
+#load packages for HIEv data
+.libPaths("C:/Users/90946112/R/win-library/3.6.2")
+
+#load packages
+library(tidyverse)
+library(here)
+library(sf)
--- data/2020/2020-01-07/plot_fires.R
+++ data/2020/2020-01-07/plot_fires.R
@@ -15,7 +15,7 @@ library(tidyverse)
 url <- "http://www.rfs.nsw.gov.au/feeds/majorIncidents.json"
 
 url %>% 
-  jsonlite::write_json("2020-01-09_incidents.json")
+  jsonlite::write_json("2020-01-10_incidents.json")
 
 fires <- st_read(url)
 
@@ -27,7 +27,19 @@ fires2<-fires %>%
   separate(Size,c("Size", "garbage"),sep=" ",remove=TRUE) %>% 
   mutate(Size=as.numeric(Size)) %>% 
   select(-garbage) %>% 
-  filter(Size>0)
+  filter(Size>0) %>% 
+  mutate(moo = map(geometry, 
+                   ~ if(length(..1[[1]]) == 1 ) {
+                     pluck(..1)
+                   } else {
+                     pluck(..1, 1)
+                   }))
+
+#separate the string of description into columns (still contains the header within the observations)
+#separate size into the actual size and all the extra text and turn the character into a numeric
+#remove all observations with zero area
+#help from Desi: dealing with geometry- which has the structure of a vector with one or two lists. If one list, it is only a point. If two lists, the second contains a nested list with unlimited lists within (a list of polygons)
+#extract (pluck) just the first list from every entry (map) in geometry, taking the whole list if there is only one (pluck(..1) indicates the whole element- the point coordinates) or just the first list if there are two (pluck(..1,1) indicates the first list within a list- the first list being the point coordinates and the second being the list of polygons)
 
 # fires2$Size<-grep(": ",x=fires2$Size,value=TRUE)
 # fires2$Size<-grep(" ",x=fires2$Size,value=TRUE)
